1460bd25f99c893cb51f54f93f3fc8fb588f735d
karhis <otsopa@gmail.com>
2020-04-28 20:20:15 +0300

    "checkstyle"

--- Sudoku/src/main/java/gui/SudokuUi.java
+++ Sudoku/src/main/java/gui/SudokuUi.java
@@ -22,8 +22,6 @@ import java.util.concurrent.TimeUnit;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.EventHandler;
-import javafx.event.EventType;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 import sudoku.main.Sudoku;
@@ -33,18 +31,18 @@ import sudoku.main.Sudoku;
  * @author Otso
  */
 public class SudokuUi extends Application {
+
     private long counter = 0;
     GridPane box;
     private Date date;
     private Timeline timeline;
-    
-    
+
     private void timer(Stage stage) {
         date = Calendar.getInstance().getTime();
         timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> {
             counter = Calendar.getInstance().getTime().getTime() - date.getTime();
             stage.setTitle(
-                        "Sudoku: " + String.valueOf(TimeUnit.SECONDS.convert(counter, TimeUnit.MILLISECONDS)));
+                    "Sudoku: " + String.valueOf(TimeUnit.SECONDS.convert(counter, TimeUnit.MILLISECONDS)));
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
@@ -52,7 +50,7 @@ public class SudokuUi extends Application {
 
     @Override
     public void start(Stage screen) {
-        
+
         Stats history = new Stats();
         Sudoku sudoku = new Sudoku(38);
         sudoku.generate();
@@ -63,45 +61,40 @@ public class SudokuUi extends Application {
         //Sudoku
         GridPane box = new GridPane();
         for (int x = 0; x < 9; x++) {
-            
+
             for (int y = 0; y < 9; y++) {
                 Button r = new Button(sudoku.returnCell(x, y));
-                if ("  ".equals(sudoku.returnCell (x, y))) {
-                                    
-                r.setOnMouseClicked((MouseEvent t) -> {
-                    if ("  ".equals(r.getText()) || "9".equals(r.getText())) {
-                        r.setText("1");
-                        
-                    }
-                    else {
-                        r.setText(String.valueOf(Integer.valueOf(r.getText())+1));
-                        
-                    }
-                }); 
-                }
-                
-                else {
+                if ("  ".equals(sudoku.returnCell(x, y))) {
+
+                    r.setOnMouseClicked((MouseEvent t) -> {
+                        if ("  ".equals(r.getText()) || "9".equals(r.getText())) {
+                            r.setText("1");
+
+                        } else {
+                            r.setText(String.valueOf(Integer.valueOf(r.getText()) + 1));
+
+                        }
+                    });
+                } else {
                     r.setStyle("-fx-background-color: #a19e9d");
                 }
-                
+
                 box.add(r, x, y);
             }
         }
 
-        
         box.setHgap(2);
         box.setVgap(2);
         box.setAlignment(Pos.CENTER);
-        box.setStyle("-fx-padding: 10;" + 
-                      "-fx-background-color: #32521a;" +
-                      "-fx-border-style: solid inside;" + 
-                      "-fx-border-width: 2;" +
-                      "-fx-border-insets: 1;" + 
-                      "-fx-border-radius: 5;" + 
-                      "-fx-border-color: black;"
+        box.setStyle("-fx-padding: 10;"
+                + "-fx-background-color: #32521a;"
+                + "-fx-border-style: solid inside;"
+                + "-fx-border-width: 2;"
+                + "-fx-border-insets: 1;"
+                + "-fx-border-radius: 5;"
+                + "-fx-border-color: black;"
         );
-        
-        
+
         //BorderPane
         BorderPane window = new BorderPane();
 
@@ -112,7 +105,7 @@ public class SudokuUi extends Application {
         //Start button
         Button start = new Button("Start");
         start.setOnAction((ActionEvent event) -> {
-            
+
             screen.setScene(game);
             timer(screen);
         });
@@ -126,10 +119,7 @@ public class SudokuUi extends Application {
         stats.getChildren().add(new Label("Games: " + String.valueOf(history.gameStats())));
         stats.getChildren().add(new Label("Won: " + String.valueOf(history.wonStats())));
         window.setBottom(stats);
-        
-        
-        
-        
+
         //Buttons
         HBox topRow = new HBox();
         Button checkAnswer = new Button("Check");
@@ -153,17 +143,6 @@ public class SudokuUi extends Application {
         window.setTop(topRow);
 
     }
-    public void compareBoards() {
-     int[][] answer = new int[9][9];
-     for (int x = 0; x < 9; x++) {  
-      for (int y = 0; y < 9; y++) {
-         
-      }   
-         
-     }
-        
-    }
-    
 
     public static void main(String[] args) {
         launch(args);
